4f1eff3994b68fd56fd8b4094acbc10e4f88c73d
Vinícius Godoy de Mendonça <vinigodoy@gmail.com>
2021-06-09 23:18:12 -0300

    Refatorado o metodo main e incluido o sayHello()

--- src/Exemplo.java
+++ src/Exemplo.java
@@ -1,5 +1,9 @@
 public class Exemplo {
-    public static void main(String[] args) {
+    public static void sayHello() {
         System.out.println("Olá mundo!");
+    }
+
+    public static void main(String[] args) {
+        sayHello();
     }
 }
